7585dd1626975d05b287f3385e8d9ce1e3803b79
DharuneswarKDe <dharun12351@gmail.com>
2024-10-22 09:22:46 +0530

    route fixed verison

---
+++ src/components/Frame8/News.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './News.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+const News = () => {
+  return (
+    <div className="newss" id='news'>
+    <h1 className='text-center mt-5 mb-5 heading2'>NEWS</h1>
+    <div>
+      <Row>
+        <Col lg="4" sm="12">
+        <div className='news-img'>
+            <img src="pic1.jpg" alt="" className='news-img1' />
+            <p className='news-content1 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ipsum voluptate ipsam aut earum officia consequuntur dolorem quas magni fugit!</p>
+        </div>
+        </Col>
+        <Col lg="4" sm="12">
+        <div className='news-img'><img src="pic1.jpg" alt="" className='news-img1' />
+        <p className='news-content1 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ipsum voluptate ipsam aut earum officia consequuntur dolorem quas magni fugit!</p>
+        </div></Col>
+        <Col lg="4" sm="12">
+        <div className='news-img'><img src="pic1.jpg" alt="" className='news-img1' />
+        <p className='news-content1 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ipsum voluptate ipsam aut earum officia consequuntur dolorem quas magni fugit!</p>
+        </div></Col>
+      </Row>
+    </div>
+    </div>
+  )
+}
+
+export default News
---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from '../../components/Navbar/Header';
+import About from '../../components/Frame2/About';
+import Firm from '../../components/Frame5/FirmPage';
+import Home from '../../components/Frame6/Home';
+import Home1 from '../../components/Frame7/Home1';
+import Footer from '../../components/Frame25/Footer';
+
+import News from '../../components/Frame8/News';
+import Whatwedo from '../../components/Frame9/Whatwedo';
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <About />
+			<Firm />
+			<Home />
+			<Home1 />
+			<News />
+			<Whatwedo />
+      <Footer />
+    </div>
+  )
+}
+
+export default Main
+
